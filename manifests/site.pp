3a8309f7e99a30c688f07383d6bacdd7b0fce4f6
noelwheezy <39569614+noelwheezy@users.noreply.github.com>
2018-07-29 22:15:51 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,10 @@ node default {
     }
 node 'puppet.wheezylab.net' {
   include role::master_server
+  file { '/root/README':
+  ensure    => file,
+  content   => $fqdn,
+  }
 }
 node /^web/ {
     include role::app_server
