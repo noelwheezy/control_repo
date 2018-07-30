a5dc09e0ca4a95ce3991a7fe9f5e15cfb0c527f3
noelwheezy <39569614+noelwheezy@users.noreply.github.com>
2018-07-30 06:55:06 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -12,7 +12,7 @@ service { 'sshd':
 ssh_authorized_key { 'root@puppet-learning.wheezylab.net':
   ensure  => present,
   user  => 'root',
-  type  => 'ssh_rsa',
+  type  => 'ssh-rsa',
   key   => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCrXaNzD0FhYq3RylPD1LADsku8r50aNuMDAB+tUbWbnGWXQ6S4qIAf6yQNG9c6G2vZZuImCKe/kuxX3kUVKwmp6NrBMeRvGXAZNO0pEeG+UkhJSGaUDr5ldJxcwh8TKXvzczII9Kwtek30WoO8TSbg+7EmFml5VqFMW0CGwP9Kt7Mf7MrYBzFl8rA//ZKed1mpUT2Fgq1xz5Ty2haaZQJ1jRqQwecnkHf27hlPCstvesV40fVbmK/3gV1E+CBBoin/0o/9eONubccz1suXRSM2OKUQj0gOsONeuRn3eEZd4bnG5fZx15E5DnQFVcscCu4nK2tTnMJhqOgt7E4Al7X1',
   }
  
