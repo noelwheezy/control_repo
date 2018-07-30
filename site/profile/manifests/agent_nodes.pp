d3aaa96b350b345f6ba56ee4a7fc312bd3595e03
noelwheezy <39569614+noelwheezy@users.noreply.github.com>
2018-07-30 20:41:21 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
  # include dockeragent
   #dockeragent::node {'web.wheezylab.net':}
   #dockeragent::node {'db.wheezylab.net':}
+  #dockeragent::node {'minetest.wheezylab.net':}
   host { 'web.wheezylab.net':
    ensure => present,
    ip     => '192.168.1.148',
@@ -11,5 +12,10 @@ class profile::agent_nodes {
    ensure => present,
    ip     => '192.168.1.147',
    host_aliases => 'db',
+   }
+   host { 'minetest.wheezylab.net':
+   ensure => present,
+   ip     => '192.168.1.149',
+   host_aliases => 'minetest',
    }
   }
