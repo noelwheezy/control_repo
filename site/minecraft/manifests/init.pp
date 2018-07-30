98ac34349e02a13b0a3570425df4c9a66765cefd
noelwheezy <39569614+noelwheezy@users.noreply.github.com>
2018-07-30 21:26:37 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -3,23 +3,31 @@ class minecraft {
 file { '/opt/minecraft':
   ensure  => directory,
   }
+  
 file { '/opt/minecraft/server.jar':
   ensure  => file,
   source  => 'https://launcher.mojang.com/mc/game/1.13/server/d0caafb8438ebd206f99930cfaecfa6c9a13dca0/server.jar',
+  before  => Service['minecraft'],
   }
+  
 package { 'default-jre':
   ensure  => present,
   }
+  
 file { '/opt/minecraft/eula.txt':
   ensure  => file,
   content => 'eula=true',
   }
+  
 file { '/etc/systemd/system/minecraft.service':
   ensure  => file,
   source  => 'puppet:///modules/minecraft/minecraft.service',
   }
+  
 service { 'minecraft':
   ensure  => running,
   enable  => true,
+  require =>  [Package['java'],File['/opt/minecraft/eula.txt'],File[/etc/systemd/system/minecraft.service']],
   }
+  
 }
